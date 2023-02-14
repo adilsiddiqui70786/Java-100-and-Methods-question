09b8f72cfe696e56d2954e451b87b1cf3dc4d316
adil <brokenaedil@gmail.com>
2023-02-14 22:36:44 +0530

    Simple loop programm

---
+++ loop.java
@@ -0,0 +1,12 @@
+
+// 16th ....simple for loop java program..
+public class loop {
+    public static void main(String[] args) 
+    {
+        int n;
+        for ( n = 1; n<=10; n++)
+        System.out.println(n);
+        //System.out.println();  // go to the next line..
+    }
+
+}
